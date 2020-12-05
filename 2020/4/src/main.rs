4126d14c1c901a15add8ee2ba35eccbd307e8ae1
Isaac Diamond <isaacd9@gmail.com>
2020-12-04 18:49:49 -0800

    Day 4

--- 2020/4/src/main.rs
+++ 2020/4/src/main.rs
@@ -8,9 +8,10 @@ struct Passport {
 }
 
 const REQUIRED: [&'static str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const EYE_COLORS: [&'static str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
 
 impl Passport {
-    fn valid<T: Fn(String) -> bool>(&self, validators: &HashMap<String, Box<T>>) -> bool {
+    fn valid(&self, validators: &HashMap<String, Box<Fn(String) -> bool>>) -> bool {
         for r in &REQUIRED {
             let data = self.fields.get(&r.to_string());
 
@@ -18,7 +19,13 @@ impl Passport {
                 Some(d) => {
                     let v = validators.get(&r.to_string());
                     match v {
-                        Some(validator) => println!("{}", validator(d.to_string())),
+                        Some(validator) => {
+                            let res = validator(d.to_string());
+                            if !res {
+                                return false;
+                            }
+                            ()
+                        }
                         None => (),
                     }
                 }
@@ -62,23 +69,79 @@ fn main() {
     let lines = stdin.lock().lines();
     let passports = parse_passports(lines).unwrap();
 
-    let mut validators: HashMap<String, Box<&dyn Fn(String) -> bool>> = HashMap::new();
+    let mut validators: HashMap<String, Box<dyn Fn(String) -> bool>> = HashMap::new();
 
     validators.insert(
         "byr".to_string(),
-        Box::new(&|st: String| {
-            println!("{}", st);
+        Box::new(|st: String| {
             let parsed = st.parse::<u32>().unwrap();
-            parsed > 1920 && parsed < 2002
+            parsed >= 1920 && parsed <= 2002
         }),
     );
 
     validators.insert(
         "iyr".to_string(),
-        Box::new(&|st: String| {
-            println!("{}", st);
+        Box::new(|st: String| {
             let parsed = st.parse::<u32>().unwrap();
-            parsed > 1920 && parsed < 2002
+            parsed >= 2010 && parsed <= 2020
+        }),
+    );
+
+    validators.insert(
+        "eyr".to_string(),
+        Box::new(|st: String| {
+            let parsed = st.parse::<u32>().unwrap();
+            parsed >= 2020 && parsed <= 2030
+        }),
+    );
+
+    validators.insert(
+        "hgt".to_string(),
+        Box::new(|st: String| {
+            let height_re = Regex::new(r"(?P<num>\d+)(?P<unit>in|cm)").unwrap();
+            let matched = height_re.captures(st.as_str());
+            match matched {
+                Some(caps) => {
+                    let parsed_num = caps["num"].parse::<u32>().unwrap();
+                    match &caps["unit"] {
+                        "cm" => parsed_num >= 150 && parsed_num <= 193,
+                        "in" => parsed_num >= 59 && parsed_num <= 76,
+                        _ => false,
+                    }
+                }
+                None => false,
+            }
+        }),
+    );
+
+    validators.insert(
+        "hcl".to_string(),
+        Box::new(|st: String| {
+            let hcl_re = Regex::new(r"#(?P<hcl>[0-9a-f]+)").unwrap();
+            let matched = hcl_re.captures(st.as_str());
+
+            match matched {
+                Some(caps) => caps["hcl"].len() == 6,
+                None => false,
+            }
+        }),
+    );
+
+    validators.insert(
+        "ecl".to_string(),
+        Box::new(|st: String| EYE_COLORS.contains(&st.as_str())),
+    );
+
+    validators.insert(
+        "pid".to_string(),
+        Box::new(|st: String| {
+            let pid_re = Regex::new(r"(?P<pid>\d+)").unwrap();
+            let matched = pid_re.captures(st.as_str());
+
+            match matched {
+                Some(caps) => caps["pid"].len() == 9,
+                None => false,
+            }
         }),
     );
 
