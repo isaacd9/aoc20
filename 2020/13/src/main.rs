d840434b5af27a348d8745396525a8767122366a
Isaac Diamond <isaacd9@gmail.com>
2020-12-13 19:25:46 -0800

    clean up

--- 2020/13/src/main.rs
+++ 2020/13/src/main.rs
@@ -31,23 +31,27 @@ fn read_input(lines: Lines<StdinLock>) -> Result<Schedule, io::Error> {
 }
 
 impl Schedule {
-    //fn find_start_time_and_bus(&self) -> (u64, u64) {
-    //    let mut start_time = self.timestamp;
-    //    let mut found_time: Option<u64> = None;
-    //    while found_time.is_none() {
-    //        for u in &self.busses {
-    //            if start_time % u == 0 {
-    //                found_time = Some(*u);
-    //            }
-    //        }
-    //        start_time += 1;
-    //    }
-    //    start_time -= 1;
-    //    return (found_time.unwrap(), start_time);
-    //}
-    //
+    fn find_start_time_and_bus(&self) -> (u64, u64) {
+        let mut start_time = self.timestamp;
+        let mut found_time: Option<u64> = None;
+        while found_time.is_none() {
+            for u in &self.busses {
+                match u {
+                    Some(t) => {
+                        if start_time % t == 0 {
+                            found_time = Some(*t);
+                        }
+                    }
+                    None => (),
+                }
+            }
+            start_time += 1;
+        }
+        start_time -= 1;
+        return (found_time.unwrap(), start_time - self.timestamp);
+    }
 
-    fn find_earliest_timestamp_by_id(&self, start_mult: u128) -> u128 {
+    fn find_earliest_timestamp_by_id(&self) -> u128 {
         let equations: Vec<(u128, u128)> = self
             .busses
             .iter()
@@ -57,7 +61,6 @@ impl Schedule {
             })
             .collect();
 
-        //println!("{:?}", equations);
         let product: u128 = equations.iter().map(|eq| eq.0).product();
 
         let mut s: u128 = 0;
@@ -72,10 +75,8 @@ impl Schedule {
                     break;
                 };
             }
-            //println!("{}x = 1 mod {}, x={}", coef, eq.0, x);
             let term = eq.1 * m * x;
             s += term;
-            //println!("{}", term);
         }
         s % product - self.busses.len() as u128 + 1
     }
@@ -86,12 +87,12 @@ fn main() {
     let lines = stdin.lock().lines();
     let sched = read_input(lines).unwrap();
 
-    let t = sched.find_earliest_timestamp_by_id(127957999999971);
-    println!("{}", t);
-
-    //println!("{} @ {}", times.0, times.1);
-    //println!(
-    //    "{} ",
-    //    times.1.checked_sub(sched.timestamp).unwrap() * times.0,
-    //);
+    let time_and_bus = sched.find_start_time_and_bus();
+    println!(
+        "{}*{} = {}",
+        time_and_bus.0,
+        time_and_bus.1,
+        time_and_bus.0 * time_and_bus.1
+    );
+    println!("{}", sched.find_earliest_timestamp_by_id());
 }
