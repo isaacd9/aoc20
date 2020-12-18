7a55a1c7dda4ad2b95f60c67fc03a51fa22f9978
Isaac Diamond <isaacd9@gmail.com>
2020-12-17 16:29:03 -0800

    wip

--- 2020/16/src/main.rs
+++ 2020/16/src/main.rs
@@ -31,25 +31,25 @@ impl Fields {
     }
 
     fn fields_matching_values(&self, values: Vec<u64>, fields: Vec<Field>) -> Vec<Field> {
-        let mut possible_fields = vec![];
-        for field in fields.iter() {
-            let mut all_matched = true;
-            for val in &values {
-                let mut some_matched = false;
-                for rule in &field.rules {
-                    if rule.contains(&val) {
-                        some_matched = true;
+        fields
+            .iter()
+            .filter(|field| {
+                let mut all_values_satisifed = true;
+                for v in &values {
+                    let mut any_rule_contains_value = false;
+
+                    for r in &field.rules {
+                        if r.contains(v) {
+                            any_rule_contains_value = true;
+                        }
                     }
+
+                    all_values_satisifed &= any_rule_contains_value
                 }
-
-                all_matched &= some_matched;
-            }
-
-            if all_matched {
-                possible_fields.push(field.clone());
-            }
-        }
-        return possible_fields;
+                all_values_satisifed
+            })
+            .map(|k| k.clone())
+            .collect()
     }
 
     fn order_fields_helper(
@@ -58,37 +58,55 @@ impl Fields {
         i: usize,
         unusued_fields: &HashMap<String, Field>,
     ) -> Option<Vec<Field>> {
+        //println!("{:?}, {:?}", i, unusued_fields);
+
         let values = tickets.iter().map(|t| t.0[i]).collect();
 
         if i == 0 {
             let rec =
                 self.fields_matching_values(values, unusued_fields.values().cloned().collect());
+            println!("base case: {}, {:?} {:?}", i, rec, unusued_fields);
             match rec.len() {
                 0 => None,
-                _ => Some(rec),
+                _ => Some(vec![rec[0].clone()]),
             }
         } else {
             let possible_fields =
                 self.fields_matching_values(values, unusued_fields.values().cloned().collect());
-            match possible_fields.len() {
-                0 => None,
-                _ => {
-                    for field in possible_fields {
-                        let mut fields_less_this_field = unusued_fields.clone();
-                        fields_less_this_field.remove(&field.name);
-                        match self.order_fields_helper(tickets, i - 1, &fields_less_this_field) {
-                            Some(rec) => {
-                                let mut r = rec.clone();
-                                r.insert(0, field.clone());
-                                return Some(r);
-                            }
-                            None => continue,
-                        }
+
+            for field in possible_fields {
+                let mut fields_less_this_field = unusued_fields.clone();
+                fields_less_this_field.remove(&field.name);
+
+                match self.order_fields_helper(tickets, i - 1, &fields_less_this_field) {
+                    Some(rec) => {
+                        let mut r = rec.clone();
+                        r.push(field.clone());
+                        return Some(r);
                     }
-                    None
+                    None => (),
                 }
             }
+
+            None
         }
+    }
+
+    fn precompute_fields(
+        &self,
+        tickets: &Vec<Ticket>,
+        fields: &Vec<Field>,
+    ) -> HashMap<usize, Vec<Field>> {
+        let mut m = HashMap::new();
+
+        for i in 0..tickets[0].0.len() {
+            let values: Vec<u64> = tickets.iter().map(|t| t.0[i]).collect();
+
+            let fields_for_i = self.fields_matching_values(values, fields.clone());
+            m.insert(i, fields_for_i);
+        }
+
+        m
     }
 
     fn order_fields(&self, t: &Vec<Ticket>) -> Vec<Field> {
@@ -96,11 +114,14 @@ impl Fields {
         for f in &self.0 {
             fields_map.insert(f.name.clone(), f.clone());
         }
-        let mut r = self
-            .order_fields_helper(t, t.len() - 1, &fields_map)
+
+        let precomputed = self.precompute_fields(t, &fields_map.values().cloned().collect());
+        println!("{:?}", precomputed);
+
+        let r = self
+            .order_fields_helper(t, t[0].0.len() - 1, &fields_map)
             .unwrap();
-        let mut k = r.clone();
-        k.reverse();
+        let k = r.clone();
         k
     }
 }
@@ -135,6 +156,25 @@ mod tests {
 
         let possible_fields = f.fields_matching_values(
             vec![9, 1, 14],
+            vec![
+                Field {
+                    name: "row".to_string(),
+                    rules: vec![(0..=5), (8..=19)],
+                },
+                Field {
+                    name: "class".to_string(),
+                    rules: vec![(0..=1), (4..=19)],
+                },
+                Field {
+                    name: "seat".to_string(),
+                    rules: vec![(0..=13), (16..=19)],
+                },
+            ],
+        );
+        println!("{:?}", possible_fields);
+
+        let possible_fields = f.fields_matching_values(
+            vec![18, 5, 9],
             vec![
                 Field {
                     name: "row".to_string(),
@@ -214,6 +254,7 @@ fn main() {
 
     let res = read_input(&mut lines);
     let fields = &res.0;
+    let my_ticket = &res.1;
     let other_tickets = &res.2;
     let mut sum = 0;
 
@@ -224,16 +265,28 @@ fn main() {
                 sum += v
             }
         }
-        println!("{:?}", sum);
+        //println!("{:?}", sum);
     }
 
-    let valid_tickets: Vec<&Ticket> = other_tickets
+    let valid_tickets: Vec<Ticket> = other_tickets
         .iter()
         .filter(|t| fields.invalid_values(t).len() == 0)
+        .cloned()
         .collect();
 
-    println!("{:?}", valid_tickets);
+    //println!("{:?}", valid_tickets);
 
-    let order = fields.order_fields(other_tickets);
-    println!("{:?}", order);
+    let order = fields.order_fields(&valid_tickets);
+    println!(
+        "{:?}",
+        order
+            .iter()
+            .map(|f| f.name.clone())
+            .collect::<Vec<String>>()
+    );
+
+    for (i, val) in my_ticket.0.iter().enumerate() {
+        let field = &order[i as usize];
+        println!("{} -> {}", field.name, val)
+    }
 }
