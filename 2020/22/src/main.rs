6e4d4224073dd434f1807cb279359fcb4d0d743a
Isaac Diamond <isaacd9@gmail.com>
2020-12-27 17:56:06 -0800

    wip

--- 2020/22/src/main.rs
+++ 2020/22/src/main.rs
@@ -23,25 +23,90 @@ impl Game {
         }
     }
 
-    fn calculate_score(&self) -> u64 {
-        let winner = if self.deck_one.len() > 0 {
+    fn calculate_score(&self) -> (u64, bool) {
+        let mut winner = true;
+        let winner_deck = if self.deck_one.len() > 0 {
+            winner = true;
             &self.deck_one
         } else {
+            winner = false;
             &self.deck_two
         };
 
-        winner
-            .iter()
-            .rev()
-            .enumerate()
-            .fold(0, |acc, (index, card)| acc + card * (index as u64 + 1))
+        (
+            winner_deck
+                .iter()
+                .rev()
+                .enumerate()
+                .fold(0, |acc, (index, card)| acc + card * (index as u64 + 1)),
+            winner,
+        )
     }
 
-    fn play(&mut self) -> u64 {
+    fn play_one_round_recursive(&mut self) {
+        //println!("{:?} {:?}", self.deck_one, self.deck_two);
+        let one_card = self.deck_one.remove(0);
+        let two_card = self.deck_two.remove(0);
+
+        let mut player_one_wins = true;
+        //println!("{} {}", one_card, two_card);
+
+        if one_card as usize <= self.deck_one.len() && two_card as usize <= self.deck_two.len() {
+            // Recursive Combat
+            //println!("RECURSIVE COMBAT!!!");
+            let mut new_game = self.clone();
+            player_one_wins = new_game.play_recursive().1;
+        } else {
+            // Regular Combat
+            if one_card > two_card {
+                player_one_wins = true;
+            } else {
+                player_one_wins = false;
+            }
+        }
+
+        if player_one_wins {
+            self.deck_one.push(one_card);
+            self.deck_one.push(two_card);
+        } else {
+            self.deck_two.push(two_card);
+            self.deck_two.push(one_card);
+        }
+    }
+
+    fn serialize_game(&self) -> String {
+        format!(
+            "{}-{}",
+            self.deck_one
+                .iter()
+                .map(|num| format!("{}", num))
+                .collect::<String>(),
+            self.deck_two
+                .iter()
+                .map(|num| format!("{}", num))
+                .collect::<String>(),
+        )
+    }
+
+    fn play_recursive(&mut self) -> (u64, bool) {
         let mut rounds = 0;
+        let mut prev_positions = HashSet::<String>::new();
+
+        while self.deck_one.len() > 0 && self.deck_two.len() > 0 {
+            self.play_one_round_recursive();
+            if prev_positions.contains(&self.serialize_game()) {
+                return (self.calculate_score().0, true);
+            }
+            rounds += 1;
+            prev_positions.insert(self.serialize_game());
+        }
+        //println!("payed {:?} rounds", rounds);
+        self.calculate_score()
+    }
+
+    fn play(&mut self) -> (u64, bool) {
         while self.deck_one.len() > 0 && self.deck_two.len() > 0 {
             self.play_one_round();
-            rounds += 1;
         }
         //println!("payed {:?} rounds", rounds);
         self.calculate_score()
@@ -68,6 +133,12 @@ fn main() {
     let lines = stdin.lock().lines();
     let mut game = read_input(lines);
 
-    let score = game.play();
+    let mut game_one = game.clone();
+    let mut game_two = game.clone();
+
+    let score = game_one.play();
+    println!("{:?}", score);
+
+    let score = game_two.play_recursive();
     println!("{:?}", score);
 }
