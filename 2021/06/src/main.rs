b9cd5ba61e1c6ebe0361440cc6195dc3d8a5de9d
Isaac Diamond <isaacd9@gmail.com>
2021-12-08 22:15:18 -0800

    part 2

--- 2021/06/src/main.rs
+++ 2021/06/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 use std::io::{self, BufRead};
 
 fn main() {
@@ -11,22 +12,26 @@ fn main() {
         .map(|st| st.parse().unwrap())
         .collect();
 
-    // for fish in input {}
-
-    // Part 1
-    println!("Initial state: {:?}", input);
-    for d in 1..=256 {
-        for fish_n in 0..input.len() {
-            if input[fish_n] == 0 {
-                input[fish_n] = 6;
-                input.push(8)
-            } else {
-                input[fish_n] -= 1;
-            }
-        }
-        println!("After {} day", d);
+    let mut m: Vec<i64> = vec![0; 9];
+    for fish_day in input {
+        m[fish_day as usize] += 1;
     }
 
-    println!("{:?}", input);
-    println!("{}", input.len());
+    // Part 1
+    println!("Initial state: {:?}", m);
+    for d in 1..=256 {
+        let mut new_m = vec![0; 9];
+        for fish_day in 0..9 {
+            if fish_day == 0 {
+                new_m[8] += m[0];
+                new_m[6] += m[0];
+            } else {
+                new_m[fish_day - 1] += m[fish_day];
+            }
+        }
+        m = new_m;
+        println!("After {} day: {:?}", d, m);
+    }
+
+    println!("{}", m.into_iter().sum::<i64>());
 }
